a9a25f059bbff9a8fd432c43d698cf40f35ad2cd
Leo <leo.freakin@gmail.com>
2026-04-24 21:53:19 +0300

    TopFold renaming

---
+++ src/Views/TopFold.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './TopFold.css';
+import CoffeeCorner from '../asset/images/Matcha-Cafe-Logo-v1.png';
+import CoffeeCornerLogo from '../asset/images/logo512.png';
+
+const TopFold = () => {
+    return (
+            <div className="top-fold-content">
+                
+                </div>
+
+    );
+};
+
+export default TopFold;
